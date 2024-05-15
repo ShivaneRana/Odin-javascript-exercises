694415c1af8879a68162cd06d1edc75de76b1829
Shivane Rana <shivanerana2004@gmail.com>
2024-05-15 18:37:28 +0530

    this one was the hardest till now

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    let sum = 0;
+		if(a < 0 || b < 0 || typeof a === "string" || typeof b === "string"){
+			return "ERROR";
+		}else if(b>a){
+			for(let i = a;i<=b;i++){
+				sum += i;
+			}
+			return sum;
+		}else if(a>b){
+			for(let i = a;i>=b;i--){
+				sum += i;
+			}
+			return sum;
+		}else{
+			return "ERROR";
+    }
 };
-
 // Do not edit below this line
 module.exports = sumAll;
