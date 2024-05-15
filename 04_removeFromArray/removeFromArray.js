9ae7f84d04ec80bde85b3d6775863b473568159b
Shivane Rana <shivanerana2004@gmail.com>
2024-05-15 10:48:04 +0530

    this one was tricky

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array,elementTOBeRemoved1,elementTOBeRemoved2,elementTOBeRemoved3,elementTOBeRemoved4,) {
+	//this one was tricky when removing multiple value consicutevly since slice would change array length leading to skipping of an iteration was able to fixed it but going through array in reverse
+    for(let i = array.length;i>=0;i--){
+		if(array[i] === elementTOBeRemoved1 || array[i] === elementTOBeRemoved2 || array[i] === elementTOBeRemoved3 || array[i] === elementTOBeRemoved4){
+			array.splice(i,1);
+		}
+	}
+	return array;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
