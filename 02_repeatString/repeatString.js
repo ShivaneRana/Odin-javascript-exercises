9a782cefd21006f68359d8143c30e9a9d28333e0
Shivane Rana <shivanerana2004@gmail.com>
2024-05-15 09:35:03 +0530

    this was easy due to the usage of repeat method

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(a,b) {
+    if(b < 0){
+        return "ERROR"
+    }
+    return a.repeat(b);
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
