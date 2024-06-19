ec6736c446cf695f85fc7b7342bb141ac1b4539d
Shivane Rana <shivanerana2004@gmail.com>
2024-06-19 22:56:01 +0530

    exercise 09 completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+	str = str.toLowerCase();
+	let arr = str.split("");
+	arr = arr.filter((item,index) => {
+		return (item>= "a" && item <= "z") || (item >= 0 && item <= 9)
+	})
+  let reversed = arr.slice().reverse().join("");
+	arr = arr.join("");
+	console.log(`Orignal array :${arr}`)
+	console.log(`Reversed array :${reversed}`)
+	return (arr === reversed);
 };
+
+console.log(palindromes("r3ace3car"));
 
 // Do not edit below this line
 module.exports = palindromes;
