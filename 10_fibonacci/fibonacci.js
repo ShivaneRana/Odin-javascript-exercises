a4859491e942dc6eda44e55c1b72a0c12e071f5b
Shivane Rana <shivanerana2004@gmail.com>
2024-06-20 14:51:51 +0530

    exercise 12 solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
+function fibonachi(num){
+	if(num < 2){
+		return num0;
+	}else{
+		let total = 0;
+		let num1 = 0;
+		let num2 = 1;
+		for(let i = 0;i < num;i++){
+			total = num1 + num2;
+			
+		}
+		return arr;
+	}
+}
 
-};
+console.log(fibonachi(6))
 
 // Do not edit below this line
 module.exports = fibonacci;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,42 @@
-const findTheOldest = function() {
+const people = [
+        {
+          name: "Carly",
+          yearOfBirth: 1066,
+        },
+        {
+          name: "Ray",
+          yearOfBirth: 1962,
+          yearOfDeath: 2011,
+        },
+        {
+          name: "Jane",
+          yearOfBirth: 1912,
+          yearOfDeath: 1941,
+        },
+      ]
+    const findTheOldest = function(people) {
+        const current = new Date().getFullYear();
+        people.forEach((item) => {
+            if(item.yearOfDeath === undefined){
+                item.yearOfDeath = current
+            }
+    })
+    const obj = people.sort((a,b) => {
+        a = a.yearOfDeath - a.yearOfBirth;
+        b = b.yearOfDeath - b.yearOfBirth;
+         return b-a;
+      })
+      
+      obj.forEach((item,index) => {
+          console.log(`${item.name} total lifespan is: ${item.yearOfDeath-item.yearOfBirth}`)
+      })
 
-};
+      return obj[0]
+ };
+
+
+console.log(findTheOldest(people))
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
